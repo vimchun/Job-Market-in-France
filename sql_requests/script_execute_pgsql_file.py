31df48d3b71555cea2b842b0dbd290f34791ad39
mhh <vimchun@gmail.com>
2025-03-17 22:50:37 +0100

    modification du script pour passer plusieurs requêtes SQL dans la sortie et de manière bien formaté

--- sql_requests/script_execute_pgsql_file.py
+++ sql_requests/script_execute_pgsql_file.py
@@ -2,22 +2,61 @@ import os
 
 import psycopg2
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+from colorama import Fore, init
+from tabulate import tabulate  # pour afficher les résultats sous forme de tableau
+
+init(autoreset=True)  # pour colorama, inutile de reset si on colorie
+
+sql_files_directory = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..",
+    "sql_requests",
+)
 
 
 def read(psql_file):
     """Simple fonction pour gagner une ligne de code pour que le code soit plus simple à écrire/lire"""
-    with open(os.path.join(current_directory, psql_file), "r") as file:
+    with open(os.path.join(sql_files_directory, psql_file), "r") as file:
         return file.read()
 
 
-query = read(os.path.join("..", "sql_requests", "total_job_offers__city_repartition.pgsql"))
+sql_files = (
+    "total_job_offers__region_repartition.pgsql",
+    "total_job_offers__department_repartition.pgsql",
+    "total_job_offers__city_repartition.pgsql",
+)
 
+queries = [(file, read(file)) for file in sql_files]
+
+# with psycopg2.connect(database="francetravail", host="localhost", user="mhh", password="mhh", port=5432) as conn:
+#     with conn.cursor() as cursor:
+#         for filename, query in queries:
+#             print(f'\n{Fore.YELLOW}===== {filename}" =====\n')
+#             cursor.execute(query)
+#             rows = cursor.fetchall()
+
+#             # Récupérer les noms des colonnes depuis cursor.description
+#             headers = [desc[0] for desc in cursor.description]
+
+#             # print(tabulate(rows, headers=headers, tablefmt="fancy_grid"), "\n")  # 'fancy_grid' donne un beau format
+#             print(tabulate(rows, headers=headers, tablefmt="psql"), "\n")  # 'fancy_grid' donne un beau format
 
 with psycopg2.connect(database="francetravail", host="localhost", user="mhh", password="mhh", port=5432) as conn:
     with conn.cursor() as cursor:
-        cursor.execute(query)
-        rows = cursor.fetchall()
+        for filename, query in queries:
+            print(f'\n{Fore.YELLOW}===== "{filename}" =====\n')
 
-        for index, row in enumerate(rows, start=1):
-            print(f"{index:03}:  {row}")
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+            # for row in rows:
+            #     print(*row, type(row))
+
+            # Récupérer les noms des colonnes
+            headers = ["#"] + [desc[0] for desc in cursor.description]
+
+            # Ajouter les numéros de ligne en première colonne
+            rows_with_index = [(index + 1, *row) for index, row in enumerate(rows)]
+
+            # Affichage du tableau avec tabulate
+            print(tabulate(rows_with_index, headers=headers, tablefmt="psql"), "\n")
