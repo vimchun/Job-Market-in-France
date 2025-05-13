b45ca409b899da1e19e5675b6027112bdfef5ba9
mhh <vimchun@gmail.com>
2025-05-13 23:58:40 +0200

    fastapi : ajout filtres région/département/ville

--- fastapi/fastapi_script.py
+++ fastapi/fastapi_script.py
@@ -2,10 +2,10 @@
 # uvicorn fastapi_script:app --reload
 # uvicorn fastapi_script:app --reload  --log-level debug
 
-
 import os
 
 from datetime import datetime
+from textwrap import dedent  # used to dedent variable strings that are indented because defined on a function
 from typing import Optional
 
 import psycopg2
@@ -51,6 +51,44 @@ def set_endpoints_filters(
         default=None,
         description='Filtrer par date de création, par exemple les offres à partir de "2025-04-25" (format "YYYY-MM-DD") _(champ vide pour ne pas filtrer)_',
     ),
+    code_region: Optional[str] = Query(
+        default=None,
+        description=dedent("""\
+            Filtrer sur le code de la région (laisser vide pour ne pas filtrer). Valeurs possibles :\n
+            • 11: Île-de-France\n
+            • 24: Centre-Val de Loire\n
+            • 27: Bourgogne-Franche-Comté\n
+            • 28: Normandie\n
+            • 32: Hauts-de-France\n
+            • 44: Grand Est\n
+            • 52: Pays de la Loire\n
+            • 53: Bretagne\n
+            • 75: Nouvelle-Aquitaine\n
+            • 76: Occitanie\n
+            • 84: Auvergne-Rhône-Alpes\n
+            • 93: Provence-Alpes-Côte d'Azur
+            """),
+    ),
+    nom_region: Optional[str] = Query(
+        default=None,
+        description="Filtrer sur le nom de la région _(champ vide pour ne pas filtrer)_",
+    ),
+    code_departement: Optional[str] = Query(
+        default=None,
+        description="Filtrer sur le code du département _(champ vide pour ne pas filtrer)_",
+    ),
+    nom_departement: Optional[str] = Query(
+        default=None,
+        description="Filtrer sur le nom du département _(champ vide pour ne pas filtrer)_",
+    ),
+    code_postal: Optional[str] = Query(
+        default=None,
+        description="Filtrer sur le code de la ville _(champ vide pour ne pas filtrer)_",
+    ),
+    nom_ville: Optional[str] = Query(
+        default=None,
+        description="Filtrer sur le nom de la ville _(champ vide pour ne pas filtrer)_",
+    ),
 ):
     # Validation de `metier_data`
     allowed_metier_data = {"DE", "DA", "DS", None}  # set
@@ -71,9 +109,39 @@ def set_endpoints_filters(
                 detail="Format invalide pour 'date_creation_min' (attendu : YYYY-MM-DD)",
             )
 
+    # Validation de `code_region`
+    if code_region:
+        pass
+
+    # Validation de `nom_region`
+    if nom_region:
+        pass
+
+    # Validation de `code_departement`
+    if code_departement:
+        pass
+
+    # Validation de `nom_departement`
+    if nom_departement:
+        pass
+
+    # Validation de `code_postal`
+    if code_postal:
+        pass
+
+    # Validation de `nom_ville`
+    if nom_ville:
+        pass
+
     return {
         "metier_data": metier_data,
         "date_creation_min": date_creation_min,
+        "code_region": code_region,
+        "nom_region": nom_region,
+        "code_departement": code_departement,
+        "nom_departement": nom_departement,
+        "code_postal": code_postal,
+        "nom_ville": nom_ville,
     }
 
 
@@ -82,6 +150,12 @@ def execute_modified_sql_request_with_filters(
     sql_files_directory_part_2,
     metier_data,
     date_creation_min,
+    code_region,
+    nom_region,
+    code_departement,
+    nom_departement,
+    code_postal,
+    nom_ville,
     fetch="all",
 ):
     """
@@ -111,6 +185,48 @@ def execute_modified_sql_request_with_filters(
             sql_file_content = sql_file_content.replace("'placeholder_date_creation_min'", "%s")
             params.append(date_creation_min)
 
+        # Filtrage par "code_region"
+        if code_region is None:
+            sql_file_content = sql_file_content.replace("AND code_region = 'placeholder_code_region'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_code_region'", "%s")
+            params.append(code_region)
+
+        # Filtrage par "nom_region"
+        if nom_region is None:
+            sql_file_content = sql_file_content.replace("AND nom_region = 'placeholder_nom_region'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_nom_region'", "%s")
+            params.append(nom_region)
+
+        # Filtrage par "code_departement"
+        if code_departement is None:
+            sql_file_content = sql_file_content.replace("AND code_departement = 'placeholder_code_departement'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_code_departement'", "%s")
+            params.append(code_departement)
+
+        # Filtrage par "nom_departement"
+        if nom_departement is None:
+            sql_file_content = sql_file_content.replace("AND nom_departement = 'placeholder_nom_departement'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_nom_departement'", "%s")
+            params.append(nom_departement)
+
+        # Filtrage par "code_postal"
+        if code_postal is None:
+            sql_file_content = sql_file_content.replace("AND code_postal = 'placeholder_code_postal'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_code_postal'", "%s")
+            params.append(code_postal)
+
+        # Filtrage par "nom_ville"
+        if nom_ville is None:
+            sql_file_content = sql_file_content.replace("AND nom_ville = 'placeholder_nom_ville'", "")
+        else:
+            sql_file_content = sql_file_content.replace("'placeholder_nom_ville'", "%s")
+            params.append(nom_ville)
+
         modified_sql_file_content = sql_file_content
 
         with psycopg2.connect(database="francetravail", host="localhost", user="mhh", password="mhh", port=5432) as conn:
@@ -125,11 +241,9 @@ def execute_modified_sql_request_with_filters(
 @app.get("/competence", tags=['Table "Competence"'])
 def get_competences(filters: dict = Depends(set_endpoints_filters)):
     """Compétences triées par code exigence ((E)xigé d'abord, puis (S)ouhaité), par nombre d'occurences (DESC) et par code (ASC)."""
-    metier_data = filters["metier_data"]
-    date_creation_min = filters["date_creation_min"]
 
     sql_file_directory_part_2 = os.path.join("01_table_Competence", "competences.pgsql")
-    result = execute_modified_sql_request_with_filters(sql_file_directory_part_2, metier_data, date_creation_min, fetch="all")
+    result = execute_modified_sql_request_with_filters(sql_file_directory_part_2, **filters, fetch="all")
 
     # tronquer la colonne "libelle" à 60 caractères pour chaque ligne, sinon le tableau s'affichera mal sur Open API
     truncated_result = [(row[0], row[1], row[2][:60] if row[2] else row[2], row[3]) for row in result]
@@ -143,12 +257,10 @@ def get_competences(filters: dict = Depends(set_endpoints_filters)):
 @app.get("/description_offre/total_offres", tags=['Table "Description_Offre"'])
 def get_number_of_offers(filters: dict = Depends(set_endpoints_filters)):
     """Nombre total d'offres d'emploi."""
-    metier_data = filters["metier_data"]
-    date_creation_min = filters["date_creation_min"]
 
     sql_file_directory_part_2 = os.path.join("10_table_DescriptionOffre", "0__total_offres.pgsql")
 
-    result = execute_modified_sql_request_with_filters(sql_file_directory_part_2, metier_data, date_creation_min, fetch="one")
+    result = execute_modified_sql_request_with_filters(sql_file_directory_part_2, **filters, fetch="one")
 
     total_offres = result[0] if result else 0
 
--- fastapi/sql_requests/10_table_DescriptionOffre/0__total_offres.pgsql
+++ fastapi/sql_requests/10_table_DescriptionOffre/0__total_offres.pgsql
@@ -3,6 +3,13 @@ SELECT
 FROM
     descriptionoffre dof
     JOIN offreemploi oe ON dof.offre_id = oe.offre_id
+    JOIN localisation l ON l.offre_id = oe.offre_id
 WHERE
-    metier_data = 'placeholder_metier_data' -- placeholder remplacé dans le script python (ou 'DE'|'DA'|'DS' si le script est exécuté en dehors de python)
-    AND date_creation >= 'placeholder_date_creation_min' -- placeholder remplacé dans le script python (ou '2025-04-10' si le script est exécuté en dehors de python)
+    metier_data = 'placeholder_metier_data'
+    AND date_creation >= 'placeholder_date_creation_min'
+    AND code_postal = 'placeholder_code_postal'
+    AND nom_ville = 'placeholder_nom_ville'
+    AND code_departement = 'placeholder_code_departement'
+    AND nom_departement = 'placeholder_nom_departement'
+    AND code_region = 'placeholder_code_region'
+    AND nom_region = 'placeholder_nom_region';
