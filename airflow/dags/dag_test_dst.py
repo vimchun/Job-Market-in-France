124713bf5f44820cc5daeed61e415fdf1fe07a91
mhh <vimchun@gmail.com>
2025-06-03 12:28:22 +0200

    début rapatriement script et fichiers nécessaires dans dossier airflow, et début correctifs avec changement des chemins des dossiers/fichiers

---
+++ airflow/dags/dag_test_dst.py
@@ -0,0 +1,25 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+my_dag = DAG(
+    dag_id="my_very_first_dag_1",
+    description="My first DAG created with DataScientest",
+    tags=["tutorial", "datascientest"],
+    schedule_interval=None,
+    default_args={
+        "owner": "airflow",
+        "start_date": days_ago(2),
+    },
+)
+
+
+# definition of the function to execute
+def print_date_and_hello():
+    print(datetime.datetime.now())
+    print("Hello from Airflow")
+
+
+my_task = PythonOperator(task_id="my_very_first_task", python_callable=print_date_and_hello, dag=my_dag)
