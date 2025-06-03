1fcf20ff45dc1f9da2afd35b9371f251f0cf74da
mhh <vimchun@gmail.com>
2025-06-03 03:01:41 +0200

    modif script bash pour faire un "docker compose down" au début et pour vérifier quand les conteneurs sont tous healthy

--- start_services.sh
+++ start_services.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-RED='\e[31m'
+GREEN='\e[32m'
 NC='\e[0m' # Reset color
+
+#### Exécution de "docker compose down"
+
+echo -e "${GREEN}\n\n== Exécution de \"docker compose down\" ${NC}"
+docker compose down
 
 #### Exécution de "docker compose up airflow-init"
 
-echo -e "${RED}== Exécution de \"docker compose up airflow-init\" ${NC}"
+echo -e "${GREEN}\n\n== Exécution de \"docker compose up airflow-init\" ${NC}"
 
-docker compose up airflow-init  # doit se terminer par "airflow-init exited with code 0"
+docker compose up airflow-init # doit se terminer par "airflow-init exited with code 0"
 if [ $? -eq 0 ]; then
     echo "✅ Initialisation réussie."
 else
@@ -17,5 +22,29 @@ fi
 
 #### Exécution de "docker compose up"
 
-echo -e "${RED}== Exécution de \"docker compose up\" ${NC}"
-docker compose up
+echo -e "${GREEN}\n\n== Exécution de \"docker compose up\" ${NC}"
+docker compose up -d
+
+#### Attente jusqu'à ce que tous les conteneurs soient healthy
+
+echo -e "${GREEN}\n\n== Attente jusqu'à ce que tous les conteneurs soient healthy ${NC}"
+
+SECONDS_WAITED=0
+
+while true; do
+    UNHEALTHY=$(docker ps --filter "health=unhealthy" --format '{{.Names}}') # retourne les conteneurs "unhealthy"
+    STARTING=$(docker ps --filter "health=starting" --format '{{.Names}}')   # retourne les conteneurs "starting"
+
+    if [ -z "$UNHEALTHY" ] && [ -z "$STARTING" ]; then
+        echo -e "${GREEN} Tous les conteneurs sont healthy.${NC}"
+        break
+    fi
+
+    MINUTES=$((SECONDS_WAITED / 60))
+    SECONDS=$((SECONDS_WAITED % 60))
+
+    printf "\r+%dm%02ds : les conteneurs ne sont pas encore tous healthy..." "$MINUTES" "$SECONDS" # formatage des secondes sur 2 digits (\r pour réécrire la ligne)
+
+    SECONDS_WAITED=$((SECONDS_WAITED + 5))
+    sleep 5
+done
