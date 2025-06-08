c61c0c60cdcc36316b30372032831c5efefaa7f0
mhh <vimchun@gmail.com>
2025-06-09 00:24:00 +0200

    suppression du nom des arguments des fonctions pour que le code soit moins volumineux et plus lisible

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -62,14 +62,14 @@ def my_dag():
         delete_json = delete_all_in_one_json()  #### task S1
 
         with TaskGroup(group_id="check_files_in_folders", tooltip="xxx") as check:
-            check_presence_csv_file(file_path=location_csv_filename)  #### task S2
-            check_presence_yaml_file(file_path=codes_appellation_filename)  #### task S2
-            count = count_json_files_number(directory_path=generated_json_files_directory)  #### task S2
+            check_presence_csv_file(location_csv_filename)  #### task S2
+            check_presence_yaml_file(codes_appellation_filename)  #### task S2
+            count = count_json_files_number(generated_json_files_directory)  #### task S2
 
         with TaskGroup(group_id="after_checks", tooltip="xxx") as after_checks:
             remove_all_json_files(json_files_original_from_api_directory)  #### task S3
             code_libelle_list = load_code_appellation_yaml_file()  #### task S3
-            token = get_bearer_token(client_id=IDENTIFIANT_CLIENT, client_secret=CLE_SECRETE, scope=SCOPES_OFFRES)  #### task S3
+            token = get_bearer_token(IDENTIFIANT_CLIENT, CLE_SECRETE, SCOPES_OFFRES)  #### task S3
 
         delete_json >> check >> after_checks
 
@@ -77,74 +77,41 @@ def my_dag():
         api_requests = (
             get_offers  #### task A1
             # "partial()" car token commun à toutes les tâches mappées
-            .partial(token=token)
+            .partial(token)
             # "expand()" car 1 task par valeur de la liste "code_libelle_list"
-            .expand(code_libelle_list=code_libelle_list)
+            .expand(code_libelle_list)
         )
 
-        all_json_in_one = concatenate_all_json_into_one(  #### task A2
-            json_files_from_api_directory=json_files_original_from_api_directory,
-            generated_json_file_directory=generated_json_files_directory,
-            new_json_filename=all_in_one_json,
-        )
+        all_json_in_one = concatenate_all_json_into_one(json_files_original_from_api_directory, generated_json_files_directory, all_in_one_json)  #### task A2
 
-        metropole = keep_only_offres_from_metropole(  #### task A3
-            json_files_directory=generated_json_files_directory,
-            json_filename=all_in_one_json,
-            new_json_filename=all_in_one_json,  # on écrase le fichier en entrée
-        )
-
-        add_location = add_location_attributes(  #### task A4
-            json_files_directory=generated_json_files_directory,
-            json_filename=all_in_one_json,
-            new_json_filename=all_in_one_json,  # on écrase le fichier en entrée
-        )
-
-        add_date_extract = add_date_extract_attribute(  #### task A5
-            json_files_directory=generated_json_files_directory,
-            json_filename=all_in_one_json,
-            new_json_filename=all_in_one_json,  # on écrase le fichier en entrée
-            date_to_insert=None,  # "None" pour avoir la date du jour
-            # date_to_insert="2025-03-02"  # pour écraser la valeur si l'attribut est existant dans le json
-        )
+        # note : pour les fonctions suivantes, on prend le fichier "all_in_one_json" et on écrase son contenu.
+        metropole = keep_only_offres_from_metropole(generated_json_files_directory, all_in_one_json, all_in_one_json)  #### task A3
+        add_location = add_location_attributes(generated_json_files_directory, all_in_one_json, all_in_one_json)  #### task A4
+        add_date_extract = add_date_extract_attribute(generated_json_files_directory, all_in_one_json, all_in_one_json, None)  #### task A5
+        # notes :
+        #   - "None" pour avoir la date du jour
+        #   - date_to_insert="2025-03-02"  pour écraser la valeur si l'attribut est existant dans le json
 
         branch = nb_json_on_setup_0_or_1(count)  #### task A6
 
         api_requests >> all_json_in_one >> metropole >> add_location >> add_date_extract >> branch
 
         with TaskGroup(group_id="0_json_in_folder", tooltip="xxx") as file0:
-            add_date_first_0 = add_date_premiere_ecriture_attribute(  #### task A8
-                json_files_directory=generated_json_files_directory,
-                json_filename=all_in_one_json,
-                new_json_filename=all_in_one_json,
-                date_to_insert=None,
-                overwrite_all_lines=False,
-            )
+            add_date_first_0 = add_date_premiere_ecriture_attribute(generated_json_files_directory, all_in_one_json, all_in_one_json, None, False)  #### task A8
 
             new_json_filename = f"{now}__extraction_occurence_1.json"
 
-            json_rename = rename_json_file(  #### task A9
-                json_files_directory=generated_json_files_directory,
-                json_filename=all_in_one_json,
-                new_json_filename=new_json_filename,
-            )
+            json_rename = rename_json_file(generated_json_files_directory, all_in_one_json, new_json_filename)  #### task A9
 
             branch >> add_date_first_0 >> json_rename
 
         with TaskGroup(group_id="1_json_in_folder", tooltip="xxx") as file1:
-            json_concat_filename = special_jsons_concatenation(generated_json_files_directory=generated_json_files_directory)  #### task A7
-
-            add_date_first_1 = add_date_premiere_ecriture_attribute(  #### task A8
-                json_files_directory=generated_json_files_directory,
-                json_filename=json_concat_filename,
-                new_json_filename=json_concat_filename,
-                date_to_insert=None,
-                overwrite_all_lines=False,
-            )
+            json_concat_filename = special_jsons_concatenation(generated_json_files_directory)  #### task A7
+            add_date_first_1 = add_date_premiere_ecriture_attribute(generated_json_files_directory, json_concat_filename, json_concat_filename, None, False)  #### task A8
 
             branch >> json_concat_filename >> add_date_first_1
 
-        write_history = write_to_history_csv_file(generated_json_files_directory=generated_json_files_directory)  #### task A10
+        write_history = write_to_history_csv_file(generated_json_files_directory)  #### task A10
 
         [file0, file1] >> write_history
 
