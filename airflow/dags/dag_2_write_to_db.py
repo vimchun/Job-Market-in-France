7aaaf6c8b07a9c1136c2d0f47af4a6258b859c7c
mhh <vimchun@gmail.com>
2025-06-13 23:12:21 +0200

    ajout d'une tâche dans un nouveau task group de setup qui vérifie qu'on a qu'un seul json avant de continuer

--- airflow/dags/dag_2_write_to_db.py
+++ airflow/dags/dag_2_write_to_db.py
@@ -1,14 +1,39 @@
+import os
+
 from airflow import DAG
+from airflow.decorators import task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.utils.task_group import TaskGroup
 
 conn_id = "my_pg"  # nom du "Connection ID" défini dans la GUI d'Airflow
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUTS_DIR = os.path.join(CURRENT_DIR, "..", "data", "outputs")
+
+AGGREGATED_JSON_DIR = os.path.join(OUTPUTS_DIR, "offres", "1--generated_json_file")
+
+
+@task(task_id="is_only_one_json")
+def check_only_one_json_file_in_folder(folder):
+    """
+    Retourne le nom du fichier json dans le dossier "folder" s'il n'y a qu'un fichier json dans ce dossier.
+    Sinon, s'il y a 0 ou plusieurs fichiers json, on arrête le script.
+    """
+    json_files = [file for file in os.listdir(folder) if file.endswith(".json")]
+    if len(json_files) == 1:
+        return json_files[0]
+    else:
+        raise Exception(f'Il y a {len(json_files)} fichier(s) json dans le dossier "{folder}": {json_files}')
+
+
 with DAG(
-    dag_id="dag_2_write_to_db",
+    dag_id="DAG_2_WRITE_TO_DB",
     tags=["project"],
 ) as dag:
-    create_tables = SQLExecuteQueryOperator(
-        conn_id=conn_id,
-        task_id="create_table",
-        sql="sql/create_all_tables.sql",
-    )
+    with TaskGroup(group_id="SETUP", tooltip="xxx") as setup:
+        create_tables = SQLExecuteQueryOperator(
+            conn_id=conn_id,
+            task_id="create_all_tables_if_not_existing",
+            sql=os.path.join("sql", "create_all_tables.sql"),
+        )
+        json_file = check_only_one_json_file_in_folder(AGGREGATED_JSON_DIR)
