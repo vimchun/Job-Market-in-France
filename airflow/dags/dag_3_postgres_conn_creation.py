d291f810dfe14cec995f4e9b37f067d14203d23b
mhh <vimchun@gmail.com>
2025-06-27 23:57:51 +0200

    dag temporaire pour la création de connection : meilleure gestion des différents cas d'erreur et écriture simplifiée du script avant intégration dans DAG 2

--- airflow/dags/dag_3_postgres_conn_creation.py
+++ airflow/dags/dag_3_postgres_conn_creation.py
@@ -2,80 +2,81 @@ from datetime import datetime, timedelta
 
 import requests
 
+from airflow.decorators import task
 from airflow.models.dag import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 
 def get_token():
+    """
+    Récupère le bearer token pour pouvoir faire les requêtes pour GET et POST les connections.
+    """
     response = requests.post(
         "http://airflow-apiserver:8080/auth/token",
         json={"username": "airflow", "password": "airflow"},
         headers={"Content-Type": "application/json"},
     )
     response.raise_for_status()
+
     return response.json()["access_token"]
 
 
-def create_postgres_conn_v2(**kwargs):
-    conn_id = kwargs["conn_id"]
+@task(task_id="check_postgres_connexion")
+def check_postgres_connexion(postgres_connection_name):
+    """
+    Vérifie que la connexion postgres nommée "postgres_connection_name" existe, et la crée le cas échéant (comme si on le faisait depuis la GUI d'Airflow).
+    """
 
-    url = f"http://airflow-apiserver:8080/api/v2/connections/{conn_id}"
+    # Requête pour récupérer les informations de la connection nommée "postgres_connection_name"
+    response = requests.get(
+        url=f"http://airflow-apiserver:8080/api/v2/connections/{postgres_connection_name}",
+        headers={"Authorization": f"Bearer {get_token()}"},
+    )
 
-    headers = {"Authorization": f"Bearer {get_token()}"}
-    response = requests.get(url, headers=headers)
+    print("** Informations connections existantes **")
+    print("   - status_code :", response.status_code)
+    print("   - text :", response.text)
 
-    print("1")
-    print(response)
-    print("Status:", response.status_code)
-    print("Text:", response.text)
-    print("Headers:", response.headers)
-
-    if response.status_code == 404:
-        payload = {
-            "connection_id": conn_id,
-            "conn_type": "postgres",
-            "host": "postgres",
-            "login": "mhh",
-            "password": "mhh",
-            "port": 5432,
-            "schema": "francetravail",
-        }
-        create_url = "http://airflow-apiserver:8080/api/v2/connections"
-
-        response = requests.post(create_url, json=payload, headers=headers)
-
-        print("2")
-        print(response)
-        print("Status:", response.status_code)
-        print("Text:", response.text)
-        print("Headers:", response.headers)
-
-    if response.status_code in [200, 201]:
-        print("Connection Created")
+    if response.status_code == 200:
+        print(f"[OK] La connection {postgres_connection_name} existe déjà.")
 
     elif response.status_code == 401:
-        print("Unauthorized. Invalid token.")
+        assert False, "Token invalide (Unauthorized)."
 
-    elif response.status_code == 200:
-        print("Connection already exists")
+    elif response.status_code == 404:
+        print(f"==> La connection {postgres_connection_name} n'existe pas, on va la créer.")
+        response_post = requests.post(
+            url="http://airflow-apiserver:8080/api/v2/connections",
+            json={
+                "connection_id": "postgres_connection",
+                "conn_type": "postgres",
+                "description": "Connection créée avec DAG 2",
+                "host": "postgres",
+                "login": "mhh",
+                "password": "mhh",
+                "port": 5432,
+                "schema": "francetravail",
+            },
+            headers={"Authorization": f"Bearer {get_token()}"},
+        )
+
+        print(f'** Création de la connection "{postgres_connection_name}" **')
+        print("   - status_code :", response_post.status_code)
+        print("   - text :", response_post.text)
+
+        assert response_post.status_code == 201, "On doit avoir status_code = 201"  # 201 si la connection est bien crée
 
     else:
-        print(f"Unexpected status code: {response.status_code}")
-        print(response.text)
+        print(f"Status code: {response.status_code}")
+        print(f"Response text : {response.text}")
+
+        assert False, f"Erreur sur la création de la connection {postgres_connection_name}"
 
 
 with DAG(
     dag_id="create_postgres_session",
-    default_args={
-        "owner": "airflow",
-        "start_date": datetime.now().replace(second=0, microsecond=0) - timedelta(minutes=1),
-    },
-    catchup=False,
+    # default_args={
+    #     "owner": "airflow",
+    #     "start_date": datetime.now().replace(second=0, microsecond=0) - timedelta(minutes=1),
+    # },
+    # catchup=False,
 ) as dag:
-    create_conn = PythonOperator(
-        task_id="create_postgres_conn_v2",
-        python_callable=create_postgres_conn_v2,
-        op_kwargs={"conn_id": "postgres_connection"},
-    )
-
-    create_conn
+    check_postgres_connexion("postgres_connection")
